f83d731e4430ec5c4884b94d92d8c4f9d67c6fb6
koojeongeun <gkje123@naver.com>
2025-03-18 19:04:01 +0900

    first commit

---
+++ src/main/java/com/example/nurseduty/NurseDutyApplication.java
@@ -0,0 +1,13 @@
+package com.example.nurseduty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseDutyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NurseDutyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nurseduty/NurseDutyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurseduty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseDutyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
